3dce0e6b0db62cc88b9eb6293d136af7ba8830c1
AsheshRanaGurung <ashesh7272@gmail.com>
2024-01-08 10:45:36 +0545

    feat: testimonial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import { Button } from '@/components/ui/button';
 import { Footer } from '../components/footer/Footer';
-
+import { Testimonial } from '../components/testimonial/Testimonial';
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-center text-2xl'>
-      Happy Coding 🙌
-      <Button>Button</Button>
+      <Testimonial />
       <Footer />
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -35,7 +35,12 @@ module.exports = {
           DEFAULT: 'hsl(var(--black))',
           foreground: 'hsl(var(--black-foreground))',
         },
-
+        blue_gray: {
+          DEFAULT: 'hsl(var(--blue_gray))',
+        },
+        aa: {
+          500: 'blue',
+        },
         destructive: {
           DEFAULT: 'hsl(var(--destructive))',
           foreground: 'hsl(var(--destructive-foreground))',
