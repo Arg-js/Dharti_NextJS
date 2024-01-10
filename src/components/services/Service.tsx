55fae7c4b31d26f47f406e436c3edf55b3e9f726
jenishakhaigulicf <jenisha.khaiguli@cloudfactory.com>
2024-01-10 14:45:42 +0545

    feat: add image in services

--- src/components/services/Service.tsx
+++ src/components/services/Service.tsx
@@ -1,13 +1,19 @@
+import Image from 'next/image';
 import React from 'react';
 
-const Service: React.FC<{ title: string; content: string }> = ({
+const Service: React.FC<{ title: string; content: string; image: string }> = ({
   title,
   content,
+  image,
 }) => {
   return (
     // todo: make the height variable
     // providing margin here would not make any difference since it is an overflowing element
-    <div className='h-[284px] w-[306px] rounded-[20px] bg-white px-10 py-[60px] text-center shadow-md'>
+    <div className='flex h-[284px] w-[306px] flex-col items-center gap-2 rounded-[20px] bg-white px-10 py-[60px] text-center shadow-md'>
+      {/* todo: can we pass className name in image */}
+      <div className='mb-4'>
+        <Image src={image} alt={title} />
+      </div>
       <p className='text-lg font-bold'>{title}</p>
       <p className='text-sm font-normal'>{content}</p>
     </div>
