548beff8992123bb5ea9c7403e2cd1a5828d61c1
jenishakhaigulicf <jenisha.khaiguli@cloudfactory.com>
2024-01-10 10:07:59 +0000

    Feat/dynamic content

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,43 @@
+import dhartiLogoIcon from './dhartiLogo.svg';
+import supportIcon from './support.svg';
+import qualityIcon from './quality.svg';
+import salesIcon from './sales.svg';
+import resultIcon from './result.svg';
+import endorsementIcon1 from './endorsement1.svg';
+import endorsementIcon2 from './endorsement2.svg';
+import endorsementIcon3 from './endorsement3.svg';
+import endorsementIcon4 from './endorsement4.svg';
+import endorsementIcon5 from './endorsement5.svg';
+import endorsementIcon6 from './endorsement6.svg';
+import heroSectionAboutBg from './heroSectionAboutBg.svg';
+import heroSectionBlogBg from './heroSectionBlogBg.svg';
+import partnerIcon1 from './partner1.svg';
+import partnerIcon2 from './partner2.svg';
+import starIcon from './star.svg';
+import testimonialImage from './testimonialImage.svg';
+import missionImage from './mission.svg';
+import aboutImage1 from './about1.svg';
+import aboutImage2 from './about2.svg';
+
+export {
+  dhartiLogoIcon,
+  supportIcon,
+  qualityIcon,
+  salesIcon,
+  resultIcon,
+  endorsementIcon1,
+  endorsementIcon2,
+  endorsementIcon3,
+  endorsementIcon4,
+  endorsementIcon5,
+  endorsementIcon6,
+  heroSectionAboutBg,
+  heroSectionBlogBg,
+  partnerIcon1,
+  partnerIcon2,
+  starIcon,
+  testimonialImage,
+  missionImage,
+  aboutImage1,
+  aboutImage2,
+};
